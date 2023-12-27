87ca141a5f602213912d4a23934ce9ed314c9aac
Mustafa Sahin <110689298+mustafaemresahin@users.noreply.github.com>
2023-12-26 22:09:17 -0600

    libraries for server code

---
+++ server.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <map>
+#include <cmath>
+#include <sstream>
+#include <string>
