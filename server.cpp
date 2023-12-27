78df00338c979ebf417fb0290cf77e6424ec7aff
Mustafa Sahin <110689298+mustafaemresahin@users.noreply.github.com>
2023-12-26 22:17:18 -0600

    initialize server address structure

--- server.cpp
+++ server.cpp
@@ -88,6 +88,12 @@ int main(int argc, char *argv[]) {
         std::cerr << "Error opening socket" << std::endl;
         exit(0);
     }
+    // Initialize server address structure
+    bzero((char *)&serv_addr, sizeof(serv_addr));
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
      
     return 0;
 }
