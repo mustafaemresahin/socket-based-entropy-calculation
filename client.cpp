09be39f795c6555ba776f3da7c9aa9853ad536ff
Mustafa Sahin <110689298+mustafaemresahin@users.noreply.github.com>
2023-12-26 21:07:07 -0600

    cast the argument inside the thread function

--- client.cpp
+++ client.cpp
@@ -22,6 +22,8 @@ struct InputData {
 
 // Thread function to handle communication with the server
 void *f(void *arg) {
+    // Cast the argument to the correct type
+    InputData *data = (InputData *)arg;
     
     return 0;
 };
